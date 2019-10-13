f013c2fbb1e7fe1bf73f00ed3485f4b92fd84f9a
Faisal <faisal-f92@hotmail.com>
2019-10-13 19:32:50 +0300

    Added `Edit` feature to meme.

--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -11,16 +11,29 @@ import UIKit
 class MemeDetailViewController: UIViewController {
     
     var memeImage: UIImage?
+    var meme: Meme?
+    var numberInList: Int?
+    
     @IBOutlet var memeImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editButtonWasPressed))
         if let memeImage = memeImage {
             memeImageView.image = memeImage
         }
         
     }
     
-
-
-
+    @objc func editButtonWasPressed() {
+        guard let meme = meme else { return }
+        guard let numberInList = numberInList else { return }
+        if let memeVC = storyboard?.instantiateViewController(withIdentifier: MemeEditorViewController_ID) as? MemeEditorViewController{
+            memeVC.editMeme = meme
+            memeVC.numberInList = numberInList
+            navigationController?.pushViewController(memeVC, animated: true)
+        }
+    }
+    
+    
+    
 }
